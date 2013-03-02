1d02420a93c5285b5d25b086eab4f14c8673c77d
Matthew Powers <matthewkevinpowers@gmail.com>
2013-03-01 19:43:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EverydayRailsTestingWithRspec::Application.config.secret_token = 'c6fee8d59b3ee1949c9588e24a6d870ae741c458f3e07a998d35f00d70f2bae5bdfa477bb62171f7d67f03d232a552029a44591bf77795212a34e8bea18f25c3'
