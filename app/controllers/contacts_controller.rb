10040f0d5b2cff7bf0fe04ef25f6eaaa2ed67d0a
Matthew Powers <matthewkevinpowers@gmail.com>
2013-03-16 11:55:19 -0400

    Added controller tests.  Last commit before adding Devise and CanCan

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,13 @@
 class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    
+  end
 end
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,5 +1,63 @@
 require 'spec_helper'
 
 describe ContactsController do
+  describe "guest access" do
+    describe "GET index" do
+      it "renders the index view" do
+        get :index
+        expect(response).to render_template :index
+      end
 
+      it "populates an array of contacts" do
+        contact = FactoryGirl.create(:contact)
+        get :index
+        expect(assigns(:contacts)).to match_array [contact]
+      end
+    end # end GET index
+
+    describe "GET show" do
+      it "renders the show view" do
+        contact = FactoryGirl.create(:contact)
+        get :show, :id => contact
+        expect(response).to render_template :show
+      end
+
+      it "assigns the requested contact to @contact" do
+        contact = FactoryGirl.create(:contact)
+        get :show, :id => contact
+        expect(assigns(:contact)).to eq contact
+      end
+    end # end GET show
+
+    describe "GET new" do
+      it "requires login" do
+        get :new
+        expect(response).to redirect_to login_url
+      end
+    end #end GET new
+
+  end #end guest access
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
